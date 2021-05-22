af17c41c2f35e24e2f4587e7d42b07d16cb02f89
Onur AKKAYA <onurakkaya26@hotmail.com>
2021-05-22 17:52:54 +0300

    Initial Commit

---
+++ getinstalled_apps.ps1
@@ -0,0 +1,28 @@
+﻿function Get-Software
+{
+    param
+    (
+        [string]
+        $DisplayName='*', 
+
+        [string]
+        $UninstallString='*'
+    )
+
+    $keys = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*', 
+            'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    
+    Get-ItemProperty -Path $keys | 
+      Where-Object { $_.DisplayName } |
+      Select-Object -Property DisplayName, DisplayVersion,Publisher, InstallDate, UninstallString |
+      Where-Object { $_.DisplayName -like $DisplayName } |
+      Where-Object { $_.UninstallString -like $UninstallString } | Export-Csv -path c:\installed_apps.csv -NoTypeInformation
+
+    Get-ItemProperty -Path $keys | 
+      Where-Object { $_.DisplayName } |
+      Select-Object -Property DisplayName, DisplayVersion,Publisher, InstallDate, UninstallString |
+      Where-Object { $_.DisplayName -like $DisplayName } |
+      Where-Object { $_.UninstallString -like $UninstallString } | Format-Table
+}
+
+Get-Software
